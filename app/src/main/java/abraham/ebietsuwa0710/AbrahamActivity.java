540dbea5174764c4cabb47cc4b60654952ba5228
AbrahamEbietsuwa0710 <kighimiebietsuwa@gmail.com>
2022-04-11 16:14:41 -0400

    i added the nav header

---
+++ app/src/main/java/abraham/ebietsuwa0710/AbrahamActivity.java
@@ -0,0 +1,15 @@
+//Abraham Ebietsuwa, N01420710, RNB
+package abraham.ebietsuwa0710;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbrahamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
