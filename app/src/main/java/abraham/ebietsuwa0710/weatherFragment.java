391e2862c4480e640774c32aad58918684c2d899
AbrahamEbietsuwa0710 <kighimiebietsuwa@gmail.com>
2022-04-14 21:15:18 -0400

    spinner added

--- app/src/main/java/abraham/ebietsuwa0710/weatherFragment.java
+++ app/src/main/java/abraham/ebietsuwa0710/weatherFragment.java
@@ -9,12 +9,35 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
-public class weatherFragment extends Fragment {
+public class weatherFragment extends Fragment  {
     @Nullable
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_weather, container, false);
+/*
+
+extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+String[] users = { "abuja", "toronto", "london", "montreal", "calgary" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), "Selected User: "+users[position] ,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
+    }
+}
+ */
     }
 }
 
