1dd18e2c98cd6398d939f2f69a755769bb24827b
Eze larry ifeanyi <ezelarry379@gmail.com>
2022-04-23 15:11:07 +0100

    assignment

--- app/src/main/java/abraham/ebietsuwa0710/downloadsFragment.java
+++ app/src/main/java/abraham/ebietsuwa0710/downloadsFragment.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.List;
 
 //import androidx.fragment.app.Fragment
 
@@ -16,7 +21,26 @@ public class downloadsFragment extends Fragment {
     @Nullable
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_downloads, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_downloads, container, false);
+
+        String[] listViewArray = {"flower","nature","mars"};
+
+        ListView listView = view.findViewById(R.id.list);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(getContext(),R.layout.list_item,listViewArray);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+
+            }
+        });
+
+        return view;
     }
 }
 
--- app/src/main/java/abraham/ebietsuwa0710/ui/home/HomeFragment.java
+++ app/src/main/java/abraham/ebietsuwa0710/ui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import abraham.ebietsuwa0710.R;
 import abraham.ebietsuwa0710.databinding.FragmentHomeBinding;
@@ -24,13 +29,21 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+//        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = inflater.inflate(R.layout.fragment_home,container,false);
+
+        TextView dateText = root.findViewById(R.id.the_date);
+
+//        DateFormat dateFormat = new SimpleDateFormat();
         Toast.makeText(getActivity(), "pppppppppppppppppppppppppppppppppppppppppppppppppppp", Toast.LENGTH_LONG).show();
 
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 
-//        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = inflater.inflate(R.layout.fragment_home,container,false);
+        dateText.setText(formatter.format(date));
 
         String preferencesName = "abrahamPreferencesName";
 
